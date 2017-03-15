88b122badd401deb5fb099da4b62e187aba0d463
zhf <zhaohfup@gmail.com>
2017-03-15 16:00:08 +0800

    my AL  practice

---
+++ src/main/java/SortStack.java
@@ -0,0 +1,13 @@
+package main.java;
+
+import java.util.Stack;
+
+/**
+ * Created by bst on 2017/3/15.
+ */
+public class SortStack {
+    public static void  sortStackByStack(Stack<Integer> stack){
+        Stack<Integer> help = new Stack<>();
+
+    }
+}
